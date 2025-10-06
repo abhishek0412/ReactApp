d69d2c873c816608d9ceed7b6fd4076d3edada28
Abhishek Choudhary <abchoudhary@microsoft.com>
2025-10-06 22:33:54 +0530

    Included design

--- myfirstapp/myfirstapp/src/index.js
+++ myfirstapp/myfirstapp/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './index.css';
 function Greeting() {
   return (<React.Fragment>
-      <Person />
-      <Message />
+
+      <BookList />
   </React.Fragment>);
 }
 
@@ -13,7 +13,37 @@ function Greeting() {
   } */
 
 const Person = () => {return <h1>First Component!</h1>;};
-const Message = () => {return <p>This is my message</p>};
+const Message = () => {return <h2>This is my message</h2>};
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Image = () => {
+  return (
+    <img className="book-image" src="https://picsum.photos/200" alt="Random Picsum Image" />
+  );
+}
+const Author = () => <h4>Abhishek Choudhary</h4>;
+
+const Title = () => <h2>Title</h2>;
+
+const Book = () => {
+  return (
+    <div className="book">
+      <Title />
+      <Author />
+      <Image />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Greeting />);
