7ad5ea73e9056ee16b1b300766ef06b48f6b1723
Abhishek Choudhary <abchoudhary@microsoft.com>
2025-10-06 21:36:14 +0530

    multiple wayys to return

--- myfirstapp/myfirstapp/src/index.js
+++ myfirstapp/myfirstapp/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-  return <h1>First Component!</h1>;
+  return <div><h1>First Component!</h1></div>;
 }
+
+/*   function Greeting() {
+    return React.createElement('div', {}, React.createElement('h1', {}, 'Hello World'));
+  } */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Greeting />);
