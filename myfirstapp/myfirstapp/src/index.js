2da1120e5c6870077f62abece8096eb32baa0799
Abhishek Choudhary <abchoudhary@microsoft.com>
2025-10-06 22:07:15 +0530

    Added Person and Mmessa

--- myfirstapp/myfirstapp/src/index.js
+++ myfirstapp/myfirstapp/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-  return <div><h1>First Component!</h1></div>;
+  return (<React.Fragment>
+      <Person />
+      <Message />
+  </React.Fragment>);
 }
 
 /*   function Greeting() {
     return React.createElement('div', {}, React.createElement('h1', {}, 'Hello World'));
   } */
+
+const Person = () => {return <h1>First Component!</h1>;};
+const Message = () => {return <p>This is my message</p>};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Greeting />);
